e04af96fb551c56a19b57236403b3fd9cf6c16fc
René Naujoks <r.naujoks@steute.com>
2021-07-29 11:44:26 +0200

    State composition with Selectors

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,8 @@
-import { ActionReducerMap } from '@ngrx/store';
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
 
 import * as fromPizzas from './pizza.reducer';
 
@@ -9,3 +13,23 @@ export interface ProductsState {
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: fromPizzas.reducer,
 };
+
+export const getProductsState =
+  createFeatureSelector<ProductsState>('products');
+
+// pizza state
+export const getPizzaState = createSelector(
+  getProductsState,
+  (state: ProductsState) => state.pizzas
+);
+
+// selectors
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizza);
+export const getPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+export const getPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
